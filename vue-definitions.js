251d43f08a8fa5957b5df5f5c231ea218734ea80
Ricardo Gonçalves <63736648+MrSpiffyClean@users.noreply.github.com>
2020-05-13 23:03:38 +0100

    Disable trendline line draw if no data is displayed

--- vue-definitions.js
+++ vue-definitions.js
@@ -754,7 +754,7 @@ window.app = new Vue({
 
       }));
 
-      if (this.showTrendLine && this.doublingTime > 0) {
+      if (this.showTrendLine && this.doublingTime > 0 && this.countries.some(country => this.selectedCountries.includes(country)) {
         let cases = [1, 10000000];
 
         let trace3 = [{
